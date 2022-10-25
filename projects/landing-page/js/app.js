2eecbcef8d86660a51262d013888a2c709078bd4
mariem walid <98715323+mariemwalid19@users.noreply.github.com>
2022-10-25 23:13:41 +0200

    Update app.js

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -60,3 +60,39 @@
 // Set sections as active
 
 
+let s1 = document.getElementById("s1");
+let sec1 = document.getElementById("section1");
+s1.onclick = function () {
+  sec1.scrollIntoView();
+};
+let s2 = document.getElementById("s2");
+let sec2 = document.getElementById("section2");
+s2.onclick = function () {
+  sec2.scrollIntoView();
+};
+let s3 = document.getElementById("s3");
+let sec3 = document.getElementById("section3");
+s3.onclick = function () {
+  sec3.scrollIntoView();
+};
+
+let userInput = document.querySelector("[name='username']");
+let ageInput = document.querySelector("[name='age']");
+
+document.forms[0].onsubmit = function (e) {
+  let userValid = false;
+  let ageValid = false;
+  if (userInput.value !== "" && userInput.value.length <= 10) {
+    userValid = true;
+  }
+  if (ageInput.value !== "") {
+    ageValid = true;
+  }
+  if (userValid === 0 || ageValid === 0) {
+    e.preventDefault();
+  }
+};
+document.links[0].onclick = function (event) {
+  console.log(event);
+  event.preventDefault();
+};
